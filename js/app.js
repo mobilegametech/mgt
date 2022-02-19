1a45fbfbb841d1296124211e7aec9dff2174794c
mobilegametech <99981446+mobilegametech@users.noreply.github.com>
2022-02-19 00:54:37 +0000

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var play = document.getElementById("play");
+
+var text = document.getElementById("timeText");
+   
+play.onclick = function(){
+
+    text.innerHTML = "32d 22hrs : 30m : 10s";
+
+    if(text.innerHTML = "32d 22hrs : 30m : 10s"){
+        alert("Coming Soon Please Stay Tune!");
+        prompt("What's your name in gaming?");
+        alert("name, Welcome to GMT");
+    }
+ 
+    text.style.color = "orangered";
+
+    text.style.fontFamily = "'Poppins', cursive";
+
+}
+
